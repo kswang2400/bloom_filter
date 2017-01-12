ce90dfe539be802fe31300f065ac4d0849359853
Kevin Wang <kevinwang2400@gmail.com>
2017-01-12 11:12:16 -0800

    cleanup

--- __init__.py
+++ __init__.py
@@ -16,9 +16,9 @@ def calculate_probability_of_false_positive():
 
 
 if __name__ == '__main__':
-    print "num hash functions: ".ljust(20) + "{depth}".format(depth=DEPTH)
+    print "\nnum hash functions: ".ljust(20) + "{depth}".format(depth=DEPTH)
     print "num inserted: ".ljust(20) + "{inserted}".format(inserted=NUM_INSERT)
-    print "num 'bits': ".ljust(20) + "{size}".format(size=SIZE)
-    print "{p} probability of false positive".format(
+    print "num 'bits': ".ljust(20) + "{size}\n".format(size=SIZE)
+    print "{p} probability of false positive\n".format(
         p=calculate_probability_of_false_positive()
     )
