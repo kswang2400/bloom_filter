6b9f2480d81a48a5a36ac4856a8c67ed45a8ac3a
Kevin Wang <kevinwang2400@gmail.com>
2017-01-12 10:37:52 -0800

    slots way slower :(

--- bloom_filter.py
+++ bloom_filter.py
@@ -1,20 +1,22 @@
 class BloomFilter(object):
-    def __init__(self):
-        self.space = [None] * 10 ** 7
+    __slots__ = tuple('a' + str(i) for i in xrange(10 ** 7))
 
     def add(self, element):
-        self.space[hash1(element)] = True
-        self.space[hash2(element)] = True
-        self.space[hash3(element)] = True
+        setattr(self, 'a' + str(hash1(element)), True)
+        setattr(self, 'a' + str(hash2(element)), True)
+        setattr(self, 'a' + str(hash3(element)), True)
 
         return True
 
     def has(self, element):
-        return (
-            self.space[hash1(element)]
-            or self.space[hash2(element)]
-            or self.space[hash3(element)]
-        )
+        try:
+            return getattr(self, 'a' + str(hash1(element)))
+            return getattr(self, 'a' + str(hash2(element)))
+            return getattr(self, 'a' + str(hash3(element)))
+        except AttributeError:
+            pass
+
+        return False
 
 def hash1(element):
     return hash(element) % 10 ** 7
